55e35759d6efb0eb48f52403f9ff78c32bff34ee
Moein-mac <moein.azizi1997@gmail.com>
2026-06-15 19:54:39 +0330

    added health check route

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,13 @@ import connectDB from "./libs/db.js";
 
 const app = express();
 
+app.get("/health",(req,res)=>{
+    res.status(200).json({
+        message : "OK",
+        ok : true
+    });
+})
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,async()=>{
