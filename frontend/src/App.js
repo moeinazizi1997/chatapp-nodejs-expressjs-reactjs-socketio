2bbe1d66026049117e4606b8a14c697124d0cf2e
Moein <moein.azizi1997@gmail.com>
2026-07-22 20:54:36 +0330

    Integrated Clerek using Clerk provider and add header section in App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>My App</h1>
+        <header>
+          <Show when="signed-out">
+            <SignInButton mode="modal"/>
+            <SignUpButton mode="modal"/>
+          </Show>
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+        </header>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ClerkProvider } from '@clerk/react';
+
+const CLERK_PUBLISHABLE_KEY = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
     <App />
-  </React.StrictMode>
+  </ClerkProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
