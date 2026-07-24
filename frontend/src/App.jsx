7449bd633eec3fff9ee00fe3a169f594eb76de26
Moein <moein.azizi1997@gmail.com>
2026-07-24 18:47:50 +0330

    implemented theme context and wallpaper context and also added some assets into public folder for sounds, wallpapers, logo, auth etc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,14 @@
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Button } from '@heroui/react';
+import {ThemeProvider} from "./context/ThemeContext";
+import {WallpaperProvider} from "./context/WallpaperContext";
 function App() {
   return(
-    <div>
-      <h1 className='text-red-500 text-5xl'>My App</h1>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal"/>
-          <SignUpButton mode="modal"/>
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+        
+      </WallpaperProvider>
+    </ThemeProvider>
   )
   
 }
