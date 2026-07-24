b56d4910f13db82f60dd2005812e8f9cc86fa2ce
Moein <moein.azizi1997@gmail.com>
2026-07-24 17:54:29 +0330

    removed publishable key from clerk provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,8 @@ import App from './App.jsx'
 
 import { ClerkProvider } from '@clerk/react'
 
-const CLERK_PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
 createRoot(document.getElementById('root')).render(
-  <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+  <ClerkProvider>
     <App />
   </ClerkProvider>
 )
